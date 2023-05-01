cba484d605db9d64d6e879b5cac3d4b5252ba5f9
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2023-05-01 16:02:56 +0200

    javascript ellenőrzés #8

---
+++ registration.js
@@ -0,0 +1,23 @@
+function validateForm() {
+    var name = document.forms["registration"]["name"].value;
+    var email = document.forms["registration"]["email"].value;
+    var phone = document.forms["registration"]["phone"].value;
+    var rating = document.forms["registration"]["rating"].value;
+    var password = document.forms["registration"]["password"].value;
+    var confirmPassword = document.forms["registration"]["confirmPassword"].value;
+
+    if (name == "" || email == "" || phone =="" || rating == "" || password == "" || confirmPassword == "") {
+      alert("Minden mezőt ki kell tölteni!");
+      return false;
+    }
+
+    if (password != confirmPassword) {
+      alert("A jelszók nem egyeznek!");
+      return false;
+    }
+
+    document.getElementById("success-message").innerHTML = "Köszönjük a jelentkezését!<br>További információkról emailben értesítjük.";
+    document.getElementById("registration-main-div").style.paddingLeft = 0;
+    document.getElementById("registration-form").style.display = "none";
+    return false;
+  }
